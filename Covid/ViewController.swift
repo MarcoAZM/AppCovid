c5301de26b33778889aacf88ab7c7ce047c3ac41
Marco Zavala <marcoazvala98@gmail.com>
2020-12-16 13:45:44 -0600

    Initial Commit

---
+++ Covid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid
+//
+//  Created by Mac14 on 16/12/20.
+//  Copyright © 2020 Zavala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
